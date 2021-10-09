96796f2a9c5880bfe826ad4c46c378a4a1b32c6e
Sameer Pusegaonkar <sameer@umd.edu>
2021-10-09 17:44:29 -0400

    Add output for bug-fix & KCachegrind to results

--- app/AnalogSensor.cpp
+++ app/AnalogSensor.cpp
@@ -2,20 +2,13 @@
 #include <numeric>
 #include <vector>
 
-AnalogSensor::AnalogSensor(unsigned int samples)
-    : mSamples(samples)
-{
-}
+AnalogSensor::AnalogSensor(unsigned int samples): mSamples(samples) {}
 
-AnalogSensor::~AnalogSensor()
-{
-}
+AnalogSensor::~AnalogSensor() {}
 
-int AnalogSensor::Read()
-{
-    std::vector<int> *readings = new std::vector<int>(mSamples, 10);
-
-    double result = std::accumulate( readings->begin(), readings->end(), 0.0 ) / readings->size();
+int AnalogSensor::Read() {
+    std::vector<int> readings(mSamples, 10);
+    double result = std::accumulate(readings.begin(), readings.end(), 0.0) / readings.size();
     return result;
 }
 
--- app/main.cpp
+++ app/main.cpp
@@ -1,3 +1,13 @@
+/**
+ * @file main.cpp
+ * @author Sameer Pusegaonkar (sameer@umd.edu)
+ * @brief 
+ * @version 0.1
+ * @date 2021-10-09
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #include <iostream>
 #include <AnalogSensor.hpp>
 
@@ -5,9 +15,8 @@ int main()
 {
     AnalogSensor lightSensor(5);
     std::cout << "Averaged sensor reading: " << lightSensor.Read() << std::endl;
-    bool terminator;
-    if( terminator )
-    {
+    bool terminator = true;
+    if ( terminator ) {
         std::cout << "DONE" << std::endl;
     }
     return 0;
